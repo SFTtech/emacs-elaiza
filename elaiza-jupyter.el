03f5bd458ee1a23e16daa6881b78f290a43987bb
Alessandro Wollek <alessandro@wollek.dev>
2024-05-02 17:21:32 +0200

    feat: elaiza jupyter
    special application to handle org-mode/python
    if we see that its essentially elaiza-chat we could merge both system prompts

---
+++ elaiza-jupyter.el
@@ -0,0 +1,37 @@
+;;; elaiza-jupyter.el --- Elaiza Jupyter assistant  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Alessandro Wollek
+;;
+;; Author: Alessandro Wollek <contact@wollek.ai>
+;; Homepage: https://github.com/SFTtech/emacs-elaiza
+;; SPDX-License-Identifier: GPL-3.0-only
+;;
+;;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Elaiza Jupyter assistant.
+;;  Uses a Python session throughout the conversation and stores Matplotlib plots to file.
+;;
+;;; Code:
+(require 'elaiza-chat)
+
+(defcustom elaiza-jupyter-system-prompt (concat elaiza-chat-system-prompt
+"\nNOTE:
+- The python src blocks should use the session `:session s'
+- Src blocks that should print something should have the results option
+  `output', e.g. ~:results output~
+- If you create a plot store it as a file. Below the source block display it
+  using the org syntax for file links. As we are using org-mode you cannot use
+  ~plt.show()~ clear the memory using the other methods available.")
+  "Extended org-mode system prompt for ELAIZA."
+  :group 'elaiza
+  :type 'string)
+
+;;;###autoload
+(defun elaiza-jupyter ()
+  (interactive)
+  (elaiza-chat 'nil 'nil elaiza-jupyter-system-prompt 'nil))
+
+(provide 'elaiza-jupyter)
+;;; elaiza-jupyter.el ends here

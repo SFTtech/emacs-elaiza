3f8805bd8eb96de09477da477fd583f398597ef2
Alessandro Wollek <alessandro@wollek.dev>
2024-05-12 17:34:09 +0200

    feat: verbose debugging
    similar to `url-http-debug`

--- elaiza-utils.el
+++ elaiza-utils.el
@@ -14,8 +14,19 @@
 ;;
 ;;; Code:
 
-(defvar elaiza-debug 't "Log for debugging purposes to *elaiza log*.")
+(defcustom elaiza-debug nil
+  "Log for debugging purposes to *elaiza log*.
 
+For debugging network connections set variable `url-debug' to non-nil."
+  :group 'elaiza
+  :type 'boolean)
 
+(defun elaiza-debug (tag &rest args)
+  "Append TAG -> ARGS to *elaiza-log* if `elaiza-debug' is non-nil."
+  (when elaiza-debug
+      (with-current-buffer (get-buffer-create "*elaiza-log*")
+      (save-excursion
+	(goto-char (point-max))
+	(insert (symbol-name tag) " -> " (apply #'format args) "\n")))))
 (provide 'elaiza-utils)
 ;;; elaiza-utils.el ends here

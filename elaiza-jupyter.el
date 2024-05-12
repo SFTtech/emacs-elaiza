80ce827dbb485b0d046661746033dbe4a408c370
Alessandro Wollek <alessandro@wollek.dev>
2024-05-12 17:34:44 +0200

    refactor

--- elaiza-jupyter.el
+++ elaiza-jupyter.el
@@ -16,20 +16,21 @@
 ;;; Code:
 (require 'elaiza-chat)
 
-(defcustom elaiza-jupyter-system-prompt (concat elaiza-chat-system-prompt
-"\nNOTE:
+(defcustom elaiza-jupyter-system-prompt
+  (concat elaiza-chat-system-prompt "\nNOTE:
 - The python src blocks should use the session `:session s'
 - Src blocks that should print something should have the results option
   `output', e.g. ~:results output~
 - If you create a plot store it as a file. Below the source block display it
   using the org syntax for file links. As we are using org-mode you cannot use
   ~plt.show()~ clear the memory using the other methods available.")
-  "Extended org-mode system prompt for ELAIZA."
+  "Extended `org-mode' system prompt for ELAIZA."
   :group 'elaiza
   :type 'string)
 
 ;;;###autoload
 (defun elaiza-jupyter ()
+  "Chat with ELAIZA with special org-mode Python instructions."
   (interactive)
   (elaiza-chat 'nil 'nil elaiza-jupyter-system-prompt 'nil))
 
